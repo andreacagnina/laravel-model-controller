b07d747426b4629f6203c8a35188a1e85ba986fb
andreacagnina <amirkal@libero.it>
2024-09-25 15:13:32 +0200

    [upd] scaffolding

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div>
+                {{-- <img src="{{ vite::asset('resources/img/') }}" alt="Logo"> --}}
+                <ul class="list-unstyled d-flex">
+                    <li>
+                        <a class="text-decoration-none" href="{{ route('homepage') }}">Home</a>
+                    </li>
+                    <li>
+                        <a class="text-decoration-none" href=""></a>
+                    </li>
+                    <li>
+                        <a class="text-decoration-none" href=""></a>
+                    </li>
+                    <li>
+                        <a class="text-decoration-none" href=""></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
